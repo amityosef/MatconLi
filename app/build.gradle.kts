fd433acf1c745ee22b67006ae9ae212bbe794ea0
עמית יוסף <amityosef28@gmail.com>
2026-02-10 18:41:47 +0200

    gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-firestore-ktx")
 }
